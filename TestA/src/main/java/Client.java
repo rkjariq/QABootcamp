c9066a7b62235b4173888dc4013c2520a214f32a
rkjariquispe <rkjariq@gmail.com>
2022-10-22 00:41:30 -0400

    add from int

---
+++ TestA/src/main/java/Client.java
@@ -0,0 +1,16 @@
+public class Client {
+
+    public static void main(String []aass){
+        int a=9;
+        int b=8;
+
+        if(a>b){
+            a=a*2;
+            System.out.println(a);
+        }else{
+            b=b*2;
+            System.out.println(b);
+        }
+
+    }
+}
